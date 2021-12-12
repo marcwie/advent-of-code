c1ea5e2612c81e64bd5def4cabec3af7931aac4f
Marc Wiedermann <marcwiedermann@posteo.de>
2021-12-12 12:19:12 +0100

    speed improvements

--- 2021/day12.py
+++ 2021/day12.py
@@ -1,7 +1,3 @@
-from collections import Counter
-import numpy as np
-
-
 def load_data(filename):
     
     with open(filename) as infile:
@@ -14,64 +10,37 @@ def load_data(filename):
     network = {node: [] for node in nodes}
     
     for v1, v2 in links:
-        network[v1].append(v2)
-        network[v2].append(v1)
+        if v1 != 'end' and v2 != 'start':
+            network[v1].append(v2)
+        if v2 != 'end' and v1 != 'start':
+            network[v2].append(v1)
 
     return network
 
 
-def run(network):
-
-    paths = [['start',],]
-    is_running = True
+def is_valid_path(path, small_caves, n_double_visits=1):
     
-    while is_running:
-        is_running = False
+    double_visits = 0
+    for cave in small_caves:
+        n_visits = path.count(cave)
 
-        # An empty list for the next iteration
-        next_step = []
+        if n_visits > 2:
+            return False
 
-        # Iterate over every currently existing path
-        for path in paths:
-            current_node = path[-1]
+        if (n_visits == 2):
+            double_visits += 1
 
-            # If the path is already at the end node store it
-            if current_node == 'end':
-                next_step.append(path)
-
-            # Otherwise we go to all next valid nodes and store that section of
-            # the path
-            else:
-                for next_node in network[current_node]:
-                    if next_node.lower() not in path:
-                        next_step.append(path + [next_node])
-                        is_running = True
-
-        paths = next_step        
-
-    return paths
-
-
-def is_valid_path(path):
-
-    if 'start' in path[1:]:
-        return False
-
-    if 'end' in path[:-1]:
-        return False
-
-    small_caves = [node for node in path if node == node.lower()] 
-    n_caves = np.array([v for v in Counter(small_caves).values()])
-    if (n_caves > 2).any() or ((n_caves == 2).sum() > 1):
-        return False
+        if double_visits > n_double_visits:
+            return False
 
     return True
     
 
-def run_part2(network):
+def run(network, n_double_visits=1):
 
     paths = [['start',],]
     is_running = True
+    small_caves = [key for key in network.keys() if key == key.lower()]
 
     while is_running:
         is_running = False
@@ -85,13 +54,13 @@ def run_part2(network):
         
             if current_node == 'end':
                 next_step.append(path)
+                continue 
 
-            else:
-               for next_node in network[current_node]:
-                    new_path = path + [next_node]
-                    if is_valid_path(new_path):
-                        next_step.append(new_path)
-                        is_running = True
+            for next_node in network[current_node]:
+                new_path = path + [next_node]
+                if is_valid_path(new_path, small_caves, n_double_visits):
+                    next_step.append(new_path)
+                    is_running = True
         
         paths = next_step        
 
@@ -101,38 +70,38 @@ def run_part2(network):
 def part1():
 
     network = load_data(filename='data/day12/test')
-    solution = len(run(network=network))
+    solution = len(run(network=network, n_double_visits=0))
     assert solution == 10
 
     network = load_data(filename='data/day12/test2')
-    solution = len(run(network=network))
+    solution = len(run(network=network, n_double_visits=0))
     assert solution == 19
 
     network = load_data(filename='data/day12/test3')
-    solution = len(run(network=network))
+    solution = len(run(network=network, n_double_visits=0))
     assert solution == 226
 
     network = load_data(filename='data/day12/input')
-    solution = len(run(network=network))
+    solution = len(run(network=network, n_double_visits=0))
     print('Part1 solution:', solution)
 
 
 def part2():
 
     network = load_data(filename='data/day12/test')
-    solution = len(run_part2(network=network))
+    solution = len(run(network=network))
     assert solution == 36
 
     network = load_data(filename='data/day12/test2')
-    solution = len(run_part2(network=network))
+    solution = len(run(network=network))
     assert solution == 103
 
     network = load_data(filename='data/day12/test3')
-    solution = len(run_part2(network=network))
+    solution = len(run(network=network))
     assert solution == 3509
         
     network = load_data(filename='data/day12/input')
-    solution = len(run_part2(network=network))
+    solution = len(run(network=network))
     print('Part2 solution:', solution)
 
  
