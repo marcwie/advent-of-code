bb398cfe84d19dfe90d00b2f44e4537f6a36b312
Marc Wiedermann <marcwiedermann@posteo.de>
2021-12-12 12:31:44 +0100

    tests for day1

--- 2021/day1.py
+++ 2021/day1.py
@@ -1,13 +1,33 @@
 import numpy as np
 import pandas as pd
 
-def run():
-    data = np.loadtxt('data/day1/input')
-    print('Part1 Solution:', (np.diff(data) > 0).sum())
 
+def solve_part1(data):
+    return (np.diff(data) > 0).sum()
+
+
+def solve_part2(data):
     df = pd.DataFrame(data)
-    print('Part2 Solution:', (df.rolling(window=3).sum().dropna().diff() > 0).sum().values[0])
-   
+    return (df.rolling(window=3).sum().dropna().diff() > 0).sum().values[0]
+
+
+def run():
+    data = np.loadtxt('data/day1/test')
+    solution = solve_part1(data)
+    assert solution == 7
+
+    data = np.loadtxt('data/day1/input')
+    solution = solve_part1(data)
+    print('Part1 Solution:', solution)
+
+    data = np.loadtxt('data/day1/test')
+    solution = solve_part2(data)
+    assert solution == 5
+
+    data = np.loadtxt('data/day1/input')
+    solution = solve_part2(data)
+    print('Part2 Solution:', solution)
+
 
 if __name__ == '__main__':
     run()
