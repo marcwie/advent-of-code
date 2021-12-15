1308acff43361200b6102ba0a00e0ebd563cee8b
Marc Wiedermann <marcwiedermann@posteo.de>
2021-12-15 13:36:24 +0100

    remove pandas and numpy dependency

--- day1.py
+++ day1.py
@@ -1,33 +1,40 @@
-import numpy as np
-import pandas as pd
+def load_data(filename):
+    
+    with open(filename) as infile:
+        data = infile.read().splitlines()
+    data = [int(value) for value in data]
+
+    return data
 
 
-def solve_part1(data):
-    return (np.diff(data) > 0).sum()
+def count_increments(data, window_size=1):
+    data = sliding_window(data, window_size=window_size)
+    return sum([data[i] < data[i+1] for i in range(len(data)-1)])
 
 
-def solve_part2(data):
-    df = pd.DataFrame(data)
-    return (df.rolling(window=3).sum().dropna().diff() > 0).sum().values[0]
+def sliding_window(data, window_size):
+    n_data = len(data)
+    return [sum(data[i:i+window_size]) for i in range(n_data + 1 -window_size)]
 
 
 def run():
-    data = np.loadtxt('data/day1/test')
-    solution = solve_part1(data)
+    data = load_data('data/day1/test')
+    solution = count_increments(data)
     assert solution == 7
 
-    data = np.loadtxt('data/day1/input')
-    solution = solve_part1(data)
+    data = load_data('data/day1/input')
+    solution = count_increments(data)
     print('Part1 Solution:', solution)
+    assert solution == 1184
 
-    data = np.loadtxt('data/day1/test')
-    solution = solve_part2(data)
+    data = load_data('data/day1/test')
+    solution = count_increments(data, window_size=3)
     assert solution == 5
 
-    data = np.loadtxt('data/day1/input')
-    solution = solve_part2(data)
+    data = load_data('data/day1/input')
+    solution = count_increments(data, window_size=3)
     print('Part2 Solution:', solution)
-
+    assert solution == 1158
 
 if __name__ == '__main__':
     run()
