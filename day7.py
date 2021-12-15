3dd6eb2abaf42da69bbf61160236f7f340ef6ed5
Marc Wiedermann <marcwiedermann@posteo.de>
2021-12-15 15:52:15 +0100

    remove numpy dependency

--- day7.py
+++ day7.py
@@ -1,31 +1,35 @@
-import numpy as np
-
-
 def load_data(filename='data/day7/test'):
-    
+
     with open(filename) as infile:
         data = infile.readline().split(',')
-
-    data = np.array(data).astype(int)
+    data = [int(n) for n in data]
+    data.sort()
 
     return data
 
 
 def weighted_consumption(data):
     
-    min_x = data.min()
-    max_x = data.max()
+    min_x = min(data)
+    max_x = max(data)
     
     consumptions = []
     for final_x in range(min_x, max_x+1):
-        distance = np.abs(data - final_x)
-        consumptions.append((.5 * distance * (distance + 1)).sum())
+        distance = [abs(d - final_x) for d in data]
+        consumptions.append(sum([d * (d + 1) // 2 for d in distance]))
     
-    return int(min(consumptions))
+    return min(consumptions)
 
+
+def median(data):
+    n_data = len(data)
+    if not (n_data % 2):
+        return sum(data[n_data//2-1:n_data//2+1]) // 2
+    else:
+        return data[n_data//2]
 
 def consumption(data):
-    return int(np.abs(data - np.median(data)).sum())
+    return sum([abs(d - median(data)) for d  in data])
 
 
 def run():
@@ -40,9 +44,11 @@ def run():
     data = load_data('data/day7/input')
     solution = consumption(data) 
     print('Part1 solution:', solution)
+    assert solution == 354129
 
     solution = weighted_consumption(data)
     print('Part2 solution:', solution)
+    assert solution == 98905973
 
 
 if __name__ == '__main__':
