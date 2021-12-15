6d46f371a3bbe31e03edc4935c7bcf39e5ecb263
Marc Wiedermann <marcwiedermann@posteo.de>
2021-12-15 15:29:24 +0100

    remove numpy dependency

--- day5.py
+++ day5.py
@@ -1,31 +1,25 @@
-import numpy as np
-
-
 def load_data(filename='data/day5/input'):
 
     with open(filename) as infile:
+
         lines = []
-        
         for line in infile.readlines():
             p0, p1 = line.replace(' ', '').replace('\n', '').split('->')
             x0, y0 = p0.split(',')
             x1, y1 = p1.split(',')
-            lines.append([x0, y0, x1, y1])
+            lines.append([int(x0), int(y0), int(x1), int(y1)])
         
-        lines = np.array(lines).astype(int)
-    
     return lines
 
 
 def filter_vertical_horizontal(lines):
-    lines = [[x0, y0, x1, y1] for x0, y0, x1, y1 in lines if (x0 == x1) or (y0 == y1)]
-    return np.array(lines)
+    return [[x0, y0, x1, y1] for x0, y0, x1, y1 in lines if (x0 == x1) or (y0 == y1)]
         
 
 def create_ocean_map(lines):
-    max_x = np.max([lines[:, 0].max(), lines[:, 2].max()])
-    max_y = np.max([lines[:, 1].max(), lines[:, 3].max()])
-    return np.zeros((max_y+1, max_x+1), dtype=int)
+    max_x = max([max([l[0] for l in lines]), max([l[2] for l in lines])])
+    max_y = max([max([l[1] for l in lines]), max([l[3] for l in lines])])
+    return [[0] * (max_x + 1) for _ in range(max_y+1)]
 
 
 def fill_ocean_map(ocean_map, lines):
@@ -34,39 +28,39 @@ def fill_ocean_map(ocean_map, lines):
         
         if (x0 == x1) and (y0 <= y1):
             for y in range(y0, y1+1):
-                ocean_map[y, x0] += 1
+                ocean_map[y][x0] += 1
 
         elif (x0 == x1) and (y0 > y1):
             for y in range(y0, y1-1, -1):
-                ocean_map[y, x0] += 1
+                ocean_map[y][x0] += 1
     
         elif (y0 == y1) and (x0 <= x1):
             for x in range(x0, x1+1):
-                ocean_map[y0, x] += 1
+                ocean_map[y0][x] += 1
 
         elif (y0 == y1) and (x0 > x1):
             for x in range(x0, x1-1, -1):
-                ocean_map[y0, x] += 1
+                ocean_map[y0][x] += 1
         
         elif (x0 <= x1) and (y0 <= y1):
             n_steps = y1 - y0 
             for step in range(n_steps+1):
-                ocean_map[y0+step, x0+step] += 1
+                ocean_map[y0+step][x0+step] += 1
 
         elif (x0 > x1) and (y0 > y1):
             n_steps = y0 - y1 
             for step in range(n_steps+1):
-                ocean_map[y0-step, x0-step] += 1
+                ocean_map[y0-step][x0-step] += 1
 
         elif (x0 <= x1) and (y0 > y1):
             n_steps = x1 - x0 
             for step in range(n_steps+1):
-                ocean_map[y0-step, x0+step] += 1
+                ocean_map[y0-step][x0+step] += 1
 
         elif (x0 > x1) and (y0 <= y1):
             n_steps = y1 - y0 
             for step in range(n_steps+1):
-                ocean_map[y0+step, x0-step] += 1
+                ocean_map[y0+step][x0-step] += 1
 
     return ocean_map
 
@@ -84,8 +78,8 @@ def solve_part1(lines):
     lines = filter_vertical_horizontal(lines=lines)
     ocean_map = create_ocean_map(lines=lines)
     ocean_map = fill_ocean_map(ocean_map=ocean_map, lines=lines)
-    n_overlaps = (ocean_map > 1).sum()
-    
+    n_overlaps = sum([n > 1 for row in ocean_map for n in row])
+
     return n_overlaps
 
 
@@ -93,7 +87,7 @@ def solve_part2(lines):
 
     ocean_map = create_ocean_map(lines=lines)
     ocean_map = fill_ocean_map(ocean_map=ocean_map, lines=lines)
-    n_overlaps = (ocean_map > 1).sum()
+    n_overlaps = sum([n > 1 for row in ocean_map for n in row])
     
     return n_overlaps
 
@@ -107,6 +101,7 @@ def run():
     lines = load_data(filename='data/day5/input')
     solution = solve_part1(lines)
     print('Solution Part1:', solution)
+    assert solution == 7142
 
     lines = load_data(filename='data/day5/test')
     solution = solve_part2(lines)
@@ -115,6 +110,7 @@ def run():
     lines = load_data(filename='data/day5/input')
     solution = solve_part2(lines)
     print('Solution Part2:', solution)
+    assert solution == 20012
 
 
 if __name__ == '__main__':
