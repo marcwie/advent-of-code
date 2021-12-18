5e03663960bce24a9f5e77fda6e7afb5e9f8fd70
Marc Wiedermann <marcwiedermann@posteo.de>
2021-12-18 18:44:20 +0100

    remove numpy dependency

--- day11.py
+++ day11.py
@@ -1,39 +1,35 @@
-import numpy as np
-
-
 def load_data(filename):
 
     with open(filename) as infile:
-
         data = infile.read().splitlines()
-    
-    data = np.array([list(row) for row in data]).astype(int)
-
-    return data
+    return [[int(entry) for entry in list(row)] for row in data] 
 
 
 def one_step(data):
-
-    data = data + 1
     
-    n_rows, n_cols = data.shape
-    embedding = np.zeros((n_rows+2, n_cols+2)) - n_rows * n_cols
-    embedding[1:n_rows+1, 1:n_cols+1] = data
+    data = [[d + 1 for d in row] for row in data]
 
+    n_rows, n_cols = len(data), len(data[0])
+    embedding = [[-n_rows * n_cols] * (n_cols + 2)]
+    for row in data:
+        embedding.append([-n_rows * n_cols] + row + [-n_rows * n_cols])
+    embedding.append([-n_rows * n_cols] * (n_cols + 2))
+    
     is_flashing = []
+    idxi, idxj = range(1, n_rows+1), range(1, n_cols+1)
 
-    while (embedding >= 10).any():
-        indices = np.array(np.where(embedding >= 10)).T
+    while sum([d >= 10 for row in embedding for d in row]):
+        indices = [[i, j] for i in idxi for j in idxj if embedding[i][j] >= 10]
         for row, col in indices:
             is_flashing.append([row, col]) 
-            embedding[row, col] = 0
+            embedding[row][col] = 0
             for nb_row in (row-1, row, row+1):
                 for nb_col in (col-1, col, col+1):
                     if [nb_row, nb_col] in is_flashing:
                         continue
-                    embedding[nb_row, nb_col] += 1
+                    embedding[nb_row][nb_col] += 1
 
-    return embedding[1:n_rows+1, 1:n_cols+1]
+    return [row[1:-1] for row in embedding[1:-1]]
 
 
 def iterate(data, n_steps=10):
@@ -41,7 +37,7 @@ def iterate(data, n_steps=10):
     n_flashes = 0
     for _ in range(n_steps):
         data = one_step(data)
-        n_flashes += (data == 0).sum()
+        n_flashes += sum([d == 0 for row in data for d in row])
        
     return n_flashes
 
@@ -49,7 +45,7 @@ def iterate(data, n_steps=10):
 def find_sync(data):
    
     steps = 0
-    while (data > 0).any():
+    while sum([d > 0 for row in data for d in row]):
         data = one_step(data)
         steps += 1
        
@@ -67,6 +63,7 @@ def run():
     data = load_data(filename='data/day11/input')
     solution = iterate(data, n_steps=100)
     print('Part1 solution:', solution)
+    assert solution == 1652
 
     data = load_data(filename='data/day11/test')
     solution = find_sync(data)
@@ -75,6 +72,7 @@ def run():
     data = load_data(filename='data/day11/input')
     solution = find_sync(data)
     print('Part2 solution:', solution)
+    assert solution == 220
 
 
 if __name__ == '__main__':
