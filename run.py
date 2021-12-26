af74e43e184dd242d181cc19f6c34a34b64a50f3
Marc Wiedermann <marcwiedermann@posteo.de>
2021-12-26 23:12:07 +0100

    finalize run

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 import day01, day02, day03, day04, day05, day06, day07, day08
 import day09, day10, day11, day12, day13, day14, day15, day16
-import day17, day18, day19, day20, day21, day22, day24, day25
+import day17, day18, day19, day20, day21, day22, day23, day24
+import day25
 import time
 
 
@@ -31,6 +32,7 @@ if __name__ == '__main__':
         day20,
         day21,
         day22,
+        day23,
         day24,
         day25,
         ]
